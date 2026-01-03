02b98392055d40597de8f26e5ca88962d1d4d16f
Stephen Booth <me@sbooth.org>
2026-01-02 18:02:42 -0600

    Add SPDX tags

--- Sources/CXXDispatchSemaphore/DispatchSemaphore.cpp
+++ Sources/CXXDispatchSemaphore/DispatchSemaphore.cpp
@@ -1,5 +1,6 @@
 //
-// Copyright © 2010-2025 Stephen F. Booth
+// SPDX-FileCopyrightText: 2010 Stephen F. Booth <contact@sbooth.dev>
+// SPDX-License-Identifier: MIT
+//
 // Part of https://github.com/sbooth/CXXDispatchSemaphore
-// MIT license
 //
--- Tests/CXXDispatchSemaphoreTests/CXXDispatchSemaphoreTests.swift
+++ Tests/CXXDispatchSemaphoreTests/CXXDispatchSemaphoreTests.swift
@@ -1,3 +1,10 @@
+//
+// SPDX-FileCopyrightText: 2025 Stephen F. Booth <contact@sbooth.dev>
+// SPDX-License-Identifier: MIT
+//
+// Part of https://github.com/sbooth/CXXDispatchSemaphore
+//
+
 import Testing
 import Foundation
 @testable import CXXDispatchSemaphore
