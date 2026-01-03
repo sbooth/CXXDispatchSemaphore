2042e33570fbd86e199897b7395f69176dec2122
Stephen Booth <me@sbooth.org>
2026-01-02 18:04:37 -0600

    Add SPDX tags (#7)

--- Sources/CXXDispatchSemaphore/include/CXXDispatchSemaphore/DispatchSemaphore.hpp
+++ Sources/CXXDispatchSemaphore/include/CXXDispatchSemaphore/DispatchSemaphore.hpp
@@ -1,7 +1,8 @@
 //
-// Copyright © 2010-2025 Stephen F. Booth
+// SPDX-FileCopyrightText: 2010 Stephen F. Booth <contact@sbooth.dev>
+// SPDX-License-Identifier: MIT
+//
 // Part of https://github.com/sbooth/CXXDispatchSemaphore
-// MIT license
 //
 
 #pragma once
